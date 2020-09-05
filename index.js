aa2382328b0f16a967c69848bf11bd7f1a4b0995
Mushfiqur Rahman <iam1mahin@gmail.com>
2020-09-05 19:40:25 +0600

    Installed Basic Packages and Setup Test SSL

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("HELLO");
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Whazzzuuppp1!!");
+});
+
+https.createServer({
+    cert: fs.readFileSync('./ssl/cert.pem'),
+    key: fs.readFileSync('./ssl/key.pem')
+}, app)
+    .listen(3000, err => console.log("LISTENING on 3000"));
