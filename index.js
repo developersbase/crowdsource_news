d9fdac38abca2cfb6c12073fe6f3b3efe8740b6d
Mushfiqur Rahman <iam1mahin@gmail.com>
2020-09-05 20:01:16 +0600

    Database Setup

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const MongoClient = require('mongodb').MongoClient;
+const dbClient = require('mongoose');
+
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
 
 const app = express();
 
+const uri = "mongodb+srv://cr_news_backend:CR_NEWS_100@crowdnewscluster0.fmmav.gcp.mongodb.net/test?retryWrites=true&w=majority"
+
 app.get('/', (req, res) => {
     res.send("Whazzzuuppp1!!");
 });
 
-https.createServer({
+const httpsServer = https.createServer({
     cert: fs.readFileSync('./ssl/cert.pem'),
     key: fs.readFileSync('./ssl/key.pem')
-}, app)
-    .listen(3000, err => console.log("LISTENING on 3000"));
+}, app);
+
+dbClient.connect(uri, {useNewUrlParser: true}).then(() => {
+    httpsServer.listen(3000, err => err ? console.error(err) : console.log("LISTENING on 3000"));
+});
