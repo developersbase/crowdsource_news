507e5cd1c47816736214adba42df4f901922ccd1
Mushfiqur Rahman <iam1mahin@gmail.com>
2020-09-05 22:57:30 +0600

    Setup Basic User signup and login.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const dbClient = require('mongoose');
+const fs = require('fs');
 
+const dbClient = require('mongoose');
 const express = require('express');
 const https = require('https');
-const fs = require('fs');
+
+const uri = process.env.MONGODB_URL;
 
 const app = express();
 
-const uri = "mongodb+srv://cr_news_backend:CR_NEWS_100@crowdnewscluster0.fmmav.gcp.mongodb.net/test?retryWrites=true&w=majority"
+const user = require('./routes/user');
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/api/user', user);
 
 app.get('/', (req, res) => {
     res.send("Whazzzuuppp1!!");
@@ -17,6 +23,8 @@ const httpsServer = https.createServer({
     key: fs.readFileSync('./ssl/key.pem')
 }, app);
 
-dbClient.connect(uri, {useNewUrlParser: true}).then(() => {
-    httpsServer.listen(3000, err => err ? console.error(err) : console.log("LISTENING on 3000"));
-});
+dbClient.connect(uri, { useNewUrlParser: true })
+    .then(() => {
+        httpsServer.listen(3000, err => err ? console.error(err) : console.log("LISTENING on 3000"));
+    })
+    .catch(error => console.error(error));
