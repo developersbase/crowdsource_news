540d880ca69394a9c4f953fa9042db93b1605317
Tausif Mahmud <tmahmud44@gmail.com>
2020-09-06 18:53:05 +0600

    post model created.

--- model/post.js
+++ model/post.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
+let Comment  = require('./comments');
+let User     = require('./user')
 
 var postSchema = new mongoose.Schema({
-
+    title:String,
+    image:String,
+    description: { type:String, default:'..' },
+    author: {
+        id: {
+           type: mongoose.Schema.Types.ObjectId,
+           ref: "User"
+        },
+        username: String,
+     },
+    comments: [
+        {
+           type: mongoose.Schema.Types.ObjectId,
+           ref: "Comment"
+        }
+     ],
+    date:{type: Date, default: Date.now} 
 });
+
 
 module.exports = mongoose.model('Post', postSchema, 'posts');
