a6fbfdf438e620d536dc84d66b70d684b575195b
asif10388 <asif.khan10388@gmail.com>
2020-09-06 23:15:38 +0600

    Added react  &  made some changes to index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /* ================================= IMPORTS ================================ */
 
-const fs = require('fs');
+const fs = require("fs");
 
-const dbClient = require('mongoose');
-const express = require('express');
-const https = require('https');
+const dbClient = require("mongoose");
+const express = require("express");
+const https = require("https");
 const methodOverride = require("method-override");
+require("dotenv").config();
 
 /* ================================== SETUP ================================= */
 
@@ -14,37 +15,48 @@ const uri = process.env.MONGODB_URL;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride("_method"));
 
 /* =============================== ROUTES ============================== */
 
-const posts = require('./routes/post');
-const user = require('./routes/user');
+const posts = require("./routes/post");
+const user = require("./routes/user");
 
-app.use('/api/posts', posts, (req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use("/api/posts", posts, (req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
-app.use('/api/user', user);
+app.use("/api/user", user);
 
 /* ================================== DEMO ================================== */
 
-app.get('/', (req, res) => {
-    res.send("Whazzzuuppp1!!");
+app.get("/", (req, res) => {
+  res.send("Whazzzuuppp1!!");
 });
 
 /* ================================= SERVER ================================= */
 
-const httpsServer = https.createServer({
-    cert: fs.readFileSync('./ssl/cert.pem'),
-    key: fs.readFileSync('./ssl/key.pem')
-}, app);
+const httpsServer = https.createServer(
+  {
+    cert: fs.readFileSync("./ssl/cert.pem"),
+    key: fs.readFileSync("./ssl/key.pem"),
+  },
+  app
+);
 
-dbClient.connect(uri, { useNewUrlParser: true })
-    .then(() => {
-        httpsServer.listen(3000, err => err ? console.error(err) : console.log("LISTENING on 3000"));
-    })
-    .catch(error => console.error(error));
+dbClient
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    httpsServer.listen(5000, (err) =>
+      err ? console.error(err) : console.log("LISTENING on 5000")
+    );
+  })
+  .catch((error) => console.error(error));
