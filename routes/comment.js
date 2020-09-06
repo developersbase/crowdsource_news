98da8150702f3670e2b3576b0c271fd593d3d2a7
Tausif Mahmud <tmahmud44@gmail.com>
2020-09-06 20:37:43 +0600

    comment model and routes added .

--- routes/comment.js
+++ routes/comment.js
@@ -5,11 +5,40 @@ const express    =    require('express')
       
 
 
-
-
-
-
-
+      router.post('/blogs/:id/comments',middleware.isLoggedIn,(req,res) => {
+            Blogs.findById(req.params.id , (err, blog) => {
+                if(err) {
+                    console.log(err);
+                    res.redirect('back')
+                } else {
+                    Comment.create(req.body.comment, (err, comment) => {
+                        if(err) {
+                            req.flash("error", "Something went wrong");
+                            console.log(err);
+                        } else {
+                            comment.author.id = req.user._id;
+                            comment.author.username = req.user.username;
+                            comment.save()
+                            blog.comments.push(comment);
+                            blog.save();
+                            req.flash("success", "Successfully added comment");
+                            res.redirect(`/blogs/${blog._id}`)
+                        }
+                    })
+                }
+            })
+        });
+        
+        router.delete('/blogs/:id/comments/:comment_id', middleware.checkCommentOwnership ,(req,res) => {
+            Comment.findByIdAndDelete(req.params.comment_id,(err,removeComment) => {
+                if(err) {
+                    res.redirect('back');
+                } else {
+                    req.flash("success", "Comment deleted");
+                    res.redirect('/blogs/' + req.params.id )
+                }
+            })
+        });
 
 
 
