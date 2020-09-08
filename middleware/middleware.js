03dd748bc6d934c8100cdf96ed2f872ea90b74b8
Tausif Mahmud <tmahmud44@gmail.com>
2020-09-08 21:32:16 +0600

    middleware code changed.

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,15 +4,11 @@ const User = require('../model/user')
 const middlewareObj = {};
 
 middlewareObj.isLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return res.status(201).send({
-            message: "User Logged In",
-        });
-    } else {
-        return res.status(400).send({
-            message: "Wrong Password"
-        });
-    }
+if(req.session && req.user.session){
+    return res.status(200).send({ message: "user authenticated" });
+} else {
+    return res.status(401).send({ message: "erro" });
+}
 };
 
 module.exports = middlewareObj;
