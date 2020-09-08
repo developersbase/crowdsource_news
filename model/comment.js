a1836d3e9b53cec864e6ad7870733594692da0b2
Mushfiqur Rahman <iam1mahin@gmail.com>
2020-09-09 00:10:37 +0600

    Implemented Replies. Fixed comment UUID

--- model/comment.js
+++ model/comment.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 var commentSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4()
+  },
   author: {
     type: mongoose.Types.ObjectId
   },
--- routes/comment.js
+++ routes/comment.js
@@ -23,4 +23,27 @@ router.put('/new', (req, res) => {
     });
 });
 
+router.put('/:commentUUID/replies/new', (req, res) => {
+    Post.findOne({'_id.uuid': req.params.postUUID}, (err, post) => {
+            if (err) return console.log(err);
+
+            let replyTargetIndex = post.comments.findIndex(comment => comment._id == req.params.commentUUID);
+
+            console.log(replyTargetIndex);
+
+            req.body.reply['author'] = ObjectId("5f53c1e08b56fe5d74080c9b");
+
+            try {
+                req.body.reply.upvote == null;
+                req.body.reply.downvote == null;
+            } catch {}
+
+            post.comments[replyTargetIndex].replies.push(req.body.reply);
+            post.save();
+
+            console.log(post);
+            res.send(post);
+        })
+})
+
 module.exports = router;
