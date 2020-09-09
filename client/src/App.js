a0805888c8fec3250826d7b841442e37d0045ca0
asif10388 <asif.khan10388@gmail.com>
2020-09-10 02:53:31 +0600

    Basic Frontend added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Feed from "./components/feed/feed.component";
+import Header from "./components/header/header.component";
+import Publish from "./components/publish/publish.component";
+import Login from "./components/login/login.component";
+import SignUp from "./components/signup/signup.component";
 
 function App() {
-  return <div className="App">BOKACHODA</div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Feed} />
+        <Route exact path="/publish" component={Publish} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
