6e3234f12d900156bb29e3804a6f547fa51d829d
asif10388 <asif.khan10388@gmail.com>
2020-09-17 01:32:54 +0600

    Added rich text editor for post writing

--- client/src/components/publish/publish.component.jsx
+++ client/src/components/publish/publish.component.jsx
@@ -1,34 +1,34 @@
 import React from "react";
-//import { Link } from "react-router-dom";
-//import PropTypes from "prop-types";
+import { Editor } from "@tinymce/tinymce-react";
 
-function Publish(props) {
-  return (
-    <section className="text-gray-700 relative bg-gray-300">
-      <div className="container px-5 py-24 mx-auto flex justify-center">
-        <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0 relative z-10">
-          <h2 className="text-gray-900 text-lg mb-1 text-center">
-            Publish an Article
-          </h2>
-          <input
-            className="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
-            placeholder="Author"
-            type="text"
-          />
-          <textarea
-            className="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none"
-            placeholder="Write..."
-          ></textarea>
+class Publish extends React.Component {
+  handleEditorChange = (e) => {
+    console.log("Content was updated:", e.target.getContent());
+  };
 
-          <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Publish
-          </button>
-        </div>
-      </div>
-    </section>
-  );
+  render() {
+    return (
+      <Editor
+        apiKey="43kg5ds4aya37y3pfp66ic3q1ybgck6d3pm0uu4nldjjpkxf"
+        initialValue="<p>Initial content</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist autolink lists link image",
+            "charmap print preview anchor help",
+            "searchreplace visualblocks code",
+            "insertdatetime media table paste wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | bold italic | \
+            alignleft aligncenter alignright | \
+            bullist numlist outdent indent | help",
+        }}
+        onChange={this.handleEditorChange}
+      />
+    );
+  }
 }
-
-Publish.propTypes = {};
 
 export default Publish;
